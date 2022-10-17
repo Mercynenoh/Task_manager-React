c8bc801d8c40a8491d20b0919c19ae60ec3df9eb
Mercynenoh <mercychepngenoh74@gmail.com>
2022-10-17 20:59:57 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export function Header (){
+    return(
+    <div className="Header" >
+    <h1>Task Tracker</h1>
+     <button className="btn">Add</button>
+    </div>
+    )
+
+}
+// const headingStyle ={
+//     color:'red', textAlign:'center'
+// }
+
+export default Header
